56da2cec2ab8265f8b6685ce09b6361cf9cd8b20
Harsh Chauhan <harsh@wolvistmedia.com>
2026-05-10 19:18:14 +0530

    fix: backend mongo + server setup done

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,27 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+require('dotenv').config();
 
-dotenv.config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// DB CONNECT
 mongoose.connect(process.env.MONGO_URI)
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+.then(() => console.log("MongoDB connected"))
+.catch(err => console.log(err));
+
+// FRONTEND
+app.use(express.static(path.join(__dirname, "../frontend")));
 
 app.get("/", (req, res) => {
-    res.send("Netflix Clone API Running");
+  res.send("API Running");
 });
 
-const authRoutes = require("./routes/authRoutes");
-app.use("/api/auth", authRoutes);
-
-const watchlistRoutes = require("./routes/watchlistRoutes");
-app.use("/api/watchlist", watchlistRoutes);
-
-app.listen(5000, () => {
-    console.log("Server running on port 5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
 });
